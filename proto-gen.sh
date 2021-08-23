6ee1709d815060b6fbab926ec07e085d18d74902
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-08-24 00:42:12 +0300

    given up for overriding google types. implemented dedicated goinsane.pbutil.* proto types.

--- proto-gen.sh
+++ proto-gen.sh
@@ -11,14 +11,13 @@ go build -mod readonly -o target/ \
   google.golang.org/protobuf/cmd/protoc-gen-go
 PATH="target/:$PATH"
 
-#rm -f -- *.pb.go
-#protoc --go_out=./ --go_opt=module="$PKG" -I proto/ proto/goinsane/pbutil/*.proto
-rm -f -- examples/*.pb.go
-protoc --go_out=./ --go_opt=module="$PKG" -I proto/ proto/goinsane/pbutil/examples/*.proto
+mkdir -p target/src/
+protoc --go_out=./target/src/ --go_opt= -I proto/ proto/google/protobuf/*.proto
+protoc --go_out=./target/src/ --go_opt= -I proto/ proto/google/protobuf/compiler/*.proto
 
 find types -depth -type f -name \*.pb.go -delete
 find types -type d -empty -delete
-#protoc --go_out=./ --go_opt=module=google.golang.org/protobuf -I proto/ proto/google/protobuf/*.proto
-#protoc --go_out=./ --go_opt=module=google.golang.org/protobuf -I proto/ proto/google/protobuf/compiler/*.proto
 protoc --go_out=./ --go_opt=module="$PKG" -I proto/ proto/goinsane/pbutil/protobuf/*.proto
-protoc --go_out=./ --go_opt=module="$PKG" -I proto/ proto/goinsane/pbutil/protobuf/compiler/*.proto
+
+rm -f -- examples/*.pb.go
+protoc --go_out=./ --go_opt=module="$PKG" -I proto/ proto/goinsane/pbutil/examples/*.proto
