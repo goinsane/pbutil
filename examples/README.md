30af8571ce271da34b53bb331a0b5ff228db84d4
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-10-21 17:17:19 +0300

    updated examples directory

--- examples/README.md
+++
@@ -1,5 +0,0 @@
-## Examples
-
-To run any example, please use the command like the following:
-
-    go run example1.go
--- examples/example1.go
+++ examples/example1.go
@@ -1,18 +1,12 @@
+//go:build examples
 // +build examples
 
 package main
 
 import (
 	"fmt"
-
-	"google.golang.org/protobuf/reflect/protoreflect"
-	"google.golang.org/protobuf/reflect/protoregistry"
-
-	_ "github.com/goinsane/pbutil/types/known/durationpb"
-	_ "google.golang.org/protobuf/types/known/durationpb"
 )
 
 func main() {
-	fmt.Println(protoregistry.GlobalTypes.FindMessageByName(protoreflect.FullName("google.protobuf.Duration")))
-	//protoregistry.GlobalTypes.RegisterMessage(protoreflect.MessageType())
+	fmt.Println("hello world")
 }
